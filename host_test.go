f1ba0516c22ca901341a58cf1145b0b8e7337859
kysee <kysee.is@gmail.com>
2021-04-02 18:33:33 +0900

    .

--- host_test.go
+++ host_test.go
@@ -4,8 +4,8 @@ import "testing"
 import "github.com/knetsim"
 
 func TestAddHost(t *testing.T) {
-	knetsim.NewHost("1.1.1.1", 10)
-	knetsim.NewHost("1.1.1.2", 10)
+	knetsim.NewServer("1.1.1.1", 10)
+	knetsim.NewServer("1.1.1.2", 10)
 
-	knetsim.PrintHosts()
+	knetsim.PrintServers()
 }
--- server.go
+++ server.go
@@ -11,19 +11,17 @@ func NewServer(laddr string, port int) (*Server, error) {
 		return nil, err
 	}
 
-	if err := AddServer(h); err != nil {
-		return nil, err
-	}
 	h.conns = make(map[string]*Conn)
-	
 	return h, nil
 }
 
-func (s *Server) Listen() {
-		
+func (s *Server) Listen() error {
+	if err := AddServer(h); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *Server) Accept() *Conn {
 
 }
-	
